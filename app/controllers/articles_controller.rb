6b02e6181fa9ec256f09d3b90b5ee9c13c8c079b
Parker Davis <parkersamueldavis@gmail.com>
2016-07-19 08:46:45 -0400

    Article show page + links

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,7 +1,12 @@
 class ArticlesController < ApplicationController
+    before_action :find_article, only: [:show]
+    
     def index
     end
     
+    def show 
+       
+   end
     
     def new
         @article = Article.new 
@@ -10,13 +15,17 @@ class ArticlesController < ApplicationController
     def create 
        @article = Article.new(article_params)
         if @article.save
-            redirect_to root_path
+            redirect_to @article
             else
             render 'new'
     end
-   
+    end
     
-    private article_params
+    private 
+        def article_params
     params.require(:article).permit(:title, :content)
 end
-end
+    def find_article 
+        @article = Article.find(params[:id])
+    end
+    end
